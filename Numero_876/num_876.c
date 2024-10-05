3d077e6e1d912630999c547eacf6dea2c076eaf5
LaurieGuan <boucherlaurie@gmail.com>
2024-10-05 00:52:04 -0400

    Amélioration de l'algorithme de base et grande réduction du nombre de branches crées, nottament toutes les branches inutiles.

--- Numero_876/num_876.c
+++ Numero_876/num_876.c
@@ -6,6 +6,8 @@ struct Arbre {
         int a;
         int b;
         int c;
+        char node;
+        char nodeParent;
 };
 
 int puissance(int base, int exposant) {
@@ -26,20 +28,53 @@ int puissance(int base, int exposant) {
 
         return reponse;
 }
+int trouverTailleNiveau(int n) {
+        int tailleNiveau;
+        if (n == 0) {
+                tailleNiveau = 1;
+        }
+        else if (n == 1) {
+                tailleNiveau = 3;
+        }
+        else {
+                tailleNiveau = 3 * puissance(2, n - 1);
+        }
+
+        return tailleNiveau;
+}
+
+char rotationType(char nodeEnfant) {
+        if (nodeEnfant == 'a') {
+                nodeEnfant = 'b';
+        }
+        else if (nodeEnfant == 'b') {
+                nodeEnfant = 'c';
+        }
+        else if (nodeEnfant == 'c') {
+                nodeEnfant = 'a';
+        }
+
+        return nodeEnfant;
+}
+/*char assignerType(char type) {
+        if (type =
+}*/
 
 
 int fonctionABC(int a, int b, int c) {
         FILE *fpt;
         fpt = fopen("Valeur1.csv", "w+");
 
-        unsigned long long int p = 0;
+        int aModif = 1;
+        int bModif = 1;
+        int cModif = 1; unsigned int p = 0;
         int n = 0;
         int tailleN = 0;
         int dp;
         int solutionTrouvee = 0;
+        char nodeEnfant;
 
         struct Arbre* ptr;
-        long int size1 = 64570080;
         int size = 10000;
         ptr = malloc(size * sizeof *ptr);
 
@@ -51,36 +86,61 @@ int fonctionABC(int a, int b, int c) {
         ptr[0].a = a;
         ptr[0].b = b;
         ptr[0].c = c;
+        ptr[0].node = 'i';
+        ptr[0].nodeParent = 'i';
 
 
-        for (n = 0; solutionTrouvee == 0 && n < 5; n++) {
-
-                dp = puissance(3, n);
+        for (n = 0; solutionTrouvee == 0 && n < 8; n++) {
+                nodeEnfant = 'a';
+                dp = trouverTailleNiveau(n);
                 tailleN += dp;
 
                 for (p = p; p < tailleN; p++) {
-                        ptr[p + dp].a = (2 * (ptr[p].b + ptr[p].c)) - ptr[p].a;
-                        ptr[p + dp].b = ptr[p].b;
-                        ptr[p + dp].c = ptr[p].c;
-                        fprintf(fpt, "Node %d, a:%d, b:%d, c:%d\n", (p+dp), ptr[p+dp].a, ptr[p+dp].b, ptr[p+dp].c); 
-                        dp++;
+                        if (!(nodeEnfant == ptr[p].node && ptr[p].node != ptr[p].nodeParent)) {
 
-                        ptr[p + dp].b = (2 * (ptr[p].a + ptr[p].c)) - ptr[p].b;
-                        ptr[p + dp].a = ptr[p].a;
-                        ptr[p + dp].c = ptr[p].c;
-                        fprintf(fpt, "Node %d, a:%d, b:%d, c:%d\n", (p+dp), ptr[p+dp].a, ptr[p+dp].b, ptr[p+dp].c); 
-                        dp++;
-
-                        ptr[p + dp].c = (2 * (ptr[p].a + ptr[p].b)) - ptr[p].c;
-                        ptr[p + dp].a = ptr[p].a;
-                        ptr[p + dp].b = ptr[p].b;
-                        fprintf(fpt, "Node %d, a:%d, b:%d, c:%d\n", (p+dp), ptr[p+dp].a, ptr[p+dp].b, ptr[p+dp].c); 
+                                aModif = (2 * (ptr[p].b + ptr[p].c)) - ptr[p].a;
+                                ptr[p + dp].a = aModif;
+                                ptr[p + dp].b = ptr[p].b;
+                                ptr[p + dp].c = ptr[p].c;
+                                ptr[p + dp].node = 'a';
+                                ptr[p + dp].nodeParent = ptr[p].node;
+                                fprintf(fpt, "Node %d %c,a:%d b:%d c:%d\n", (p+dp), ptr[p+dp].node, ptr[p+dp].a, ptr[p+dp].b, ptr[p+dp].c);
+                                dp++;
+                        }
+                        nodeEnfant = rotationType(nodeEnfant);
 
 
-                        if (ptr[p + dp - 2].a == 0 || ptr[p + dp - 1].b == 0 || ptr[p + dp].c == 0) {
+                        if (!(nodeEnfant == ptr[p].node && ptr[p].node != ptr[p].nodeParent)) {
+                                bModif = (2 * (ptr[p].a + ptr[p].c)) - ptr[p].b;
+                                ptr[p + dp].b = bModif;
+                                ptr[p + dp].a = ptr[p].a;
+                                ptr[p + dp].c = ptr[p].c;
+                                ptr[p + dp].node = 'b';
+                                ptr[p + dp].nodeParent = ptr[p].node;
+                                fprintf(fpt, "Node %d %c,a:%d b:%d c:%d\n", (p+dp), ptr[p+dp].node, ptr[p+dp].a, ptr[p+dp].b, ptr[p+dp].c); 
+                                dp++;
+                        }
+                        nodeEnfant = rotationType(nodeEnfant);
+
+                        if (!(nodeEnfant == ptr[p].node && ptr[p].node != ptr[p].nodeParent)) {
+                                cModif = (2 * (ptr[p].a + ptr[p].b)) - ptr[p].c;
+                                ptr[p + dp].c = cModif;
+                                ptr[p + dp].a = ptr[p].a;
+                                ptr[p + dp].b = ptr[p].b;
+                                ptr[p + dp].node = 'c';
+                                ptr[p + dp].nodeParent = ptr[p].node;
+                                fprintf(fpt, "Node %d %c,a:%d b:%d c:%d\n", (p+dp), ptr[p+dp].node, ptr[p+dp].a, ptr[p+dp].b, ptr[p+dp].c);
+                        }
+                        else {
+                                dp--;
+                        }
+                        nodeEnfant = rotationType(nodeEnfant);
+
+                        if (aModif == 0 || bModif == 0 || cModif == 0) {
                                 solutionTrouvee = 1;
                         }
                 }
+                fprintf(fpt, "\n");
         } 
 
         if (solutionTrouvee == 0) {
@@ -125,7 +185,8 @@ int fonctionABFromFIle(int a, int b) {
         int somme = 0;
         int k;
 
-        for (k = 1; k < 19; k++) {
+        for (k = 1; k < 19
+        ; k++) {
                 somme += fonctionAB(puissance(a, k), puissance(b, k));
         }
 
@@ -133,7 +194,17 @@ int fonctionABFromFIle(int a, int b) {
 }*/
 
 int main() {
-        printf("%d", fonctionABC(6, 10, 36));
+        int i;
+
+        printf("%d", fonctionABC(6, 423, 42));
+        FILE *Test;
+        Test = fopen("TestFonctionsTailleNiveau.csv", "w+");
+
+        for (i = 0; i <= 15; i++) {
+                fprintf(Test, "Taille niveau;%d\n", trouverTailleNiveau(i));
+        }
+
+        fclose(Test);
         return 0;
 
 }
